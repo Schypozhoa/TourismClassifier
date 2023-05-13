e8046a08aa4701bc913bdcb66c8edecaf80e497c
Danu Adi Nugroho <gaul.gaul5@gmail.com>
2023-05-13 23:03:39 +0700

    Add function to download the image from the link

--- Data/ImageScrapper.py
+++ Data/ImageScrapper.py
@@ -1,27 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import time
+import requests
 import base64
 import os
 
 # Dependencies:
 # selenium          (pypi, pip install selenium)
+# requests          (pypi, pip install requests)
 # chrome-driver     (https://chromedriver.chromium.org/downloads) or use the provided one in the repository
 #
 # How to use:
 # 1. Install selenium and chrome-driver
-# 2. Change the browser_path variable to your browser executable path
-# 3. Change the search_term variable to your search term
-# 4. Change the savePath variable to your save path
-# 5. Run the script
+# 2. Change the SEARCH_TERM variable to your search term
+# 3. Change the IMAGE_PATH variable to your save path
+# 4. Change the BROWSER_PATH variable to your browser executable path
+# 5. Change the NUM_OF_IMAGES variable to the number of images you want to download
+# 6. Run the script
 #
 # (https://github.com/Schypozhoa)
 
 def init():
     # Setting the Chrome driver to find Browser executable
-    browser_path = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
     options = Options()
-    options.binary_location = browser_path
+    options.binary_location = BROWSER_PATH
 
     # Define the browser
     browser = webdriver.Chrome(options = options)
@@ -41,15 +42,14 @@ def searchImage(browser, search_term):
         browser.get(links + term)
 
         # Iterate every image from the first list
-        for index in range(1, 2):
+        for index in range(1, NUM_OF_IMAGES+1):
             image = browser.find_element("xpath", imgPathStart + str(index) + imgPathEnd)
             data = image.get_attribute("src")
             saveImage(data, term, index)
-            time.sleep(3)
 
 def saveImage(data, term, index):
     # Define save path and image name
-    savePath = "C:/Users/Administrator/Desktop/Capstone/Data/AttractionDataset/" + term + "/"
+    savePath = IMAGE_PATH + term + "/"
     imageName = str(index) + ".jpg"
 
     # Check the path exist or not
@@ -60,7 +60,7 @@ def saveImage(data, term, index):
     if checkBase64(data):
         saveBase64Image(data, savePath+imageName,)
     else:
-        print("Base64 not detected")
+        saveLinkImage(data, savePath+imageName)
 
 
 def checkBase64(data):
@@ -70,14 +70,23 @@ def checkBase64(data):
         return False
 
 def saveBase64Image(data, path):
+    print("Converting base64 to image...")
     with open(path, "wb") as fh:
         fh.write(base64.urlsafe_b64decode(data[22:]))
-    print("Image saved to " + path + " successfully")
+    print("Image saved to " + path + " successfully\n")
 
-def saveLinkImage(link):
-    pass
+def saveLinkImage(link, path):
+    print("Downloading image...")
+    response = requests.get(link)
+    with open(path, "wb") as fh:
+        fh.write(response.content)
+    print("Image saved to " + path + " successfully\n")
 
 if __name__ == "__main__":
-    browser = init()
+    NUM_OF_IMAGES = 3
+    SEARCH_TERM = ["cat","dog"]
+    IMAGE_PATH = "C:/Users/Administrator/Desktop/Capstone/Data/AttractionDataset/"
+    BROWSER_PATH = "C:/Program Files/BraveSoftware/Brave-Browser/Application/brave.exe"
 
-    searchImage(browser, ["cat","dog"])
+    browser = init()
+    searchImage(browser, SEARCH_TERM)
